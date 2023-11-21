2490b1389ba5229c74dc21c93b54b88adc32f254
link <bjhhopesin@gmail.com>
2023-11-21 22:43:56 +0900

    중간 단계

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { RestaurnatsModule } from './restaurnats/restaurnats.module';
+import { ApolloDriver } from '@nestjs/apollo';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    GraphQLModule.forRoot({
+      driver: ApolloDriver,
+      autoSchemaFile: true,
+    }),
+    RestaurnatsModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
